96afbfe260a5a819b91039f6b593bf372d46f0e5
samithau <34501597+samithau@users.noreply.github.com>
2023-10-26 23:33:02 +0530

    Update Controller.java

--- src/main/java/com/example/project/Controller.java
+++ src/main/java/com/example/project/Controller.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
